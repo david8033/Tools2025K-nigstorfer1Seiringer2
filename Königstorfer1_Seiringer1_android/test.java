ded3f446104a4ed35c99c82a06a7db79e2e61c1c
Davs_Hub <s2410237015@fhooe.at>
2025-05-30 16:10:33 +0200

    added new line of code in method

--- Königstorfer1_Seiringer1_android/test.java
+++ Königstorfer1_Seiringer1_android/test.java
@@ -1,8 +1,9 @@
 public void thisIsaNewMethod() {
-    
+    System.out.println("This is a new test line in the file.")
 }
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hallo Welt!");
     }
 }
+
