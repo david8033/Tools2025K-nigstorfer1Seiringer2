86b2299de18182a743257fb5df3473f0e86c6f99
Davs_Hub <s2410237015@fhooe.at>
2025-05-30 16:28:40 +0200

    added println to see variable

--- Königstorfer1_Seiringer1_android/test.java
+++ Königstorfer1_Seiringer1_android/test.java
@@ -2,6 +2,7 @@ public void thisIsaNewMethod() {
     int x;
     System.out.println("This is a new test line in the file.")
     x=5;
+    System.out.println("The value of x is: " + x);
 }
 public class HelloWorld {
     public static void main(String[] args) {
